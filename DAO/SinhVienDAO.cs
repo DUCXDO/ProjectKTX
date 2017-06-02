2b8b05f99fc896f9cfc7053bdc555d426593a5c0
DUCXDO <nguyenminhduc9197@gmail.com>
2017-06-02 14:50:57 +0700

    quản lý ký túc xá
    đây là code project quản lý ký túc xá

---
+++ DAO/SinhVienDAO.cs
@@ -0,0 +1,111 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DAO
+{
+    public interface ISinhVienDAO
+    {
+        SINHVIEN ThemSV(SINHVIEN sv);
+        SINHVIEN XoaSV(SINHVIEN sv);
+        SINHVIEN SuaSV(SINHVIEN sv);
+        IEnumerable<SINHVIEN> TimSV(SINHVIEN sv);
+        IEnumerable<SINHVIEN> TimTatCaSV();
+    }
+
+    public class SinhVienDAO : ISinhVienDAO
+    {
+        public SINHVIEN ThemSV(SINHVIEN sv)
+        {
+            try
+            {
+                // khai báo và khởi tạo đối tượng kết nối với database
+                KTXEntities KTXe = new KTXEntities();
+                //Thêm mới 
+                SINHVIEN result = KTXe.SINHVIENs.Add(sv);
+                //Lưu thay đổi
+                KTXe.SaveChanges();
+                //trả về đối tượng mới thêm để xác định kết quả
+                return result;
+            }
+            catch (Exception e)
+            {
+                //trả về null để biết là có lỗi
+                return null;
+            }
+        }
+
+        public SINHVIEN XoaSV(SINHVIEN sv)
+        {
+            try
+            {
+                KTXEntities KTXe = new KTXEntities();
+                SINHVIEN delete = KTXe.SINHVIENs.Find(sv.MaSV);
+                SINHVIEN result = KTXe.SINHVIENs.Remove(delete);
+                return result;
+            }
+            catch (Exception e)
+            {
+                return null;
+            }
+        }
+
+        public SINHVIEN SuaSV(SINHVIEN sv)
+        {
+            try
+            {
+                KTXEntities KTXe = new KTXEntities();
+                SINHVIEN edit = KTXe.SINHVIENs.Find(sv);
+                edit.NgaySinh = sv.NgaySinh;
+                edit.SoCMND = sv.SoCMND;
+                edit.SoDT = sv.SoDT;
+                edit.TenSV = sv.TenSV;
+                edit.DiaChi = sv.DiaChi;
+                int result = KTXe.SaveChanges();
+                if (result == 1)
+                { return edit; }
+                else
+                { return null; }
+            }
+            catch (Exception e)
+            {
+                return null;
+            }
+        }
+
+        public IEnumerable<SINHVIEN> TimTatCaSV()
+        {
+            try
+            {
+                KTXEntities KTXe = new KTXEntities();
+                IEnumerable<SINHVIEN> result = KTXe.SINHVIENs.AsEnumerable();
+                return result;
+            }
+            catch (Exception e)
+            {
+                return null;
+            }
+        }
+
+        public IEnumerable<SINHVIEN> TimSV(SINHVIEN sv)
+        {
+            try
+            {
+                KTXEntities KTXe = new KTXEntities();
+                IEnumerable<SINHVIEN> result = KTXe.SINHVIENs.AsQueryable().Where(x =>
+                (sv.MaSV == null || x.MaSV == sv.MaSV) &&
+                (sv.NgaySinh == null || x.NgaySinh == sv.NgaySinh) &&
+                (sv.SoCMND == 0) || (x.SoCMND == sv.SoCMND) &&
+                (sv.SoDT == null) || (x.SoDT == sv.SoDT));
+                return result;
+            }catch(Exception e)
+            {
+                return null;
+            }
+        }
+    }
+
+
+}
---
+++ ProjectKTX/Form1.Designer.cs
@@ -0,0 +1,69 @@
+﻿namespace ProjectKTX
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.monoFlat_ThemeContainer_Bright1 = new MonoFlat.MonoFlat_ThemeContainer_Bright();
+            this.SuspendLayout();
+            // 
+            // monoFlat_ThemeContainer_Bright1
+            // 
+            this.monoFlat_ThemeContainer_Bright1.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(32)))), ((int)(((byte)(41)))), ((int)(((byte)(50)))));
+            this.monoFlat_ThemeContainer_Bright1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.monoFlat_ThemeContainer_Bright1.Font = new System.Drawing.Font("Segoe UI", 9F);
+            this.monoFlat_ThemeContainer_Bright1.Location = new System.Drawing.Point(0, 0);
+            this.monoFlat_ThemeContainer_Bright1.Name = "monoFlat_ThemeContainer_Bright1";
+            this.monoFlat_ThemeContainer_Bright1.Padding = new System.Windows.Forms.Padding(10, 70, 10, 9);
+            this.monoFlat_ThemeContainer_Bright1.RoundCorners = true;
+            this.monoFlat_ThemeContainer_Bright1.Sizable = true;
+            this.monoFlat_ThemeContainer_Bright1.Size = new System.Drawing.Size(853, 513);
+            this.monoFlat_ThemeContainer_Bright1.SmartBounds = true;
+            this.monoFlat_ThemeContainer_Bright1.StartPosition = System.Windows.Forms.FormStartPosition.WindowsDefaultLocation;
+            this.monoFlat_ThemeContainer_Bright1.TabIndex = 0;
+            this.monoFlat_ThemeContainer_Bright1.Text = "monoFlat_ThemeContainer_Bright1";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(853, 513);
+            this.Controls.Add(this.monoFlat_ThemeContainer_Bright1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
+            this.Name = "Form1";
+            this.Text = "monoFlat_ThemeContainer_Bright1";
+            this.TransparencyKey = System.Drawing.Color.Fuchsia;
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private MonoFlat.MonoFlat_ThemeContainer_Bright monoFlat_ThemeContainer_Bright1;
+    }
+}
+
